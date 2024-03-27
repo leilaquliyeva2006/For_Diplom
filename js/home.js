f7761e4b56067f2a6ad120bf9859e2e996730604
leila <leylag659@gmail.com>
2024-03-27 19:03:04 +0400

    add js and some function

---
+++ js/home.js
@@ -0,0 +1,38 @@
+let cartIcon = document.querySelector("#cart-icon");
+let cart = document.querySelector(".cart");
+let closeCart = document.querySelector("#close-cart");
+
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+function ready() {
+  let removeCartButtons = document.getElementsByClassName("cart-remove");
+
+  for (let i = 0; i < removeCartButtons.length; i++) {
+    let button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+  let quantityInputs = document.getElementsByClassName("cart-quantity");
+
+  for (let i = 0; i < quantityInputs.length; i++) {
+    let input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+  let addCart = document.getElementsByClassName("add-cart");
+  for (let i = 0; i < addCart.length; i++) {
+    let button = addCart[i];
+    button.addEventListener("click", addCartClecked);
+  }
+}
+
