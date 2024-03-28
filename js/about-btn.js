64a660f9cad926d079b0a593293f1e11ee512dea
leila <leylag659@gmail.com>
2024-03-28 15:06:37 +0400

    product3 added

--- js/about-btn.js
+++ js/about-btn.js
@@ -1,9 +1,10 @@
-document.getElementById("product1").addEventListener("click", function() {
-    window.location.href = "product1.html";
-  });
-  
+document.getElementById("product1").addEventListener("click", function () {
+  window.location.href = "product1.html";
+});
 
-  document.getElementById("product2").addEventListener("click", function() {
-    window.location.href = "product2.html";
-  });
-  
+document.getElementById("product2").addEventListener("click", function () {
+  window.location.href = "product2.html";
+});
+document.getElementById("product3").addEventListener("click", function () {
+  window.location.href = "product3.html";
+});
