168dee60503afd04ae52921fe6fd4f94c33bb108
leila <leylag659@gmail.com>
2024-03-28 15:36:10 +0400

    for gallery js

---
+++ js/slider.js
@@ -0,0 +1,30 @@
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const images = document.querySelectorAll(".gallery img");
+let currentIndex = 0;
+
+prevButton.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateGallery();
+});
+
+nextButton.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateGallery();
+});
+
+function updateGallery() {
+  images.forEach((img, index) => {
+    if (index === currentIndex) {
+      img.classList.add("selected");
+    } else {
+      img.classList.remove("selected");
+    }
+  });
+}
+
+
+
+document.querySelector(".home-button").addEventListener("click", function() {
+    window.location.href = "home.html";
+});
