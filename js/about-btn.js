531abd1e9cc2b3168c6c91d49d2af99adf96b3d7
leila <leylag659@gmail.com>
2024-03-28 15:02:36 +0400

    product2 added

--- js/about-btn.js
+++ js/about-btn.js
@@ -2,3 +2,8 @@ document.getElementById("product1").addEventListener("click", function() {
     window.location.href = "product1.html";
   });
   
+
+  document.getElementById("product2").addEventListener("click", function() {
+    window.location.href = "product2.html";
+  });
+  
